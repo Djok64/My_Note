fa231e2ee034f212c72caaf33b4819ee7d417979
Djok64 <joakym.anc@gmail.com>
2023-08-29 10:57:14 +0200

    explication et commentaire sur le async  await et .end dans le fichier test.js qui lui contrairement a databaseConnexionUniqueOld.js met fin a la connexion avec le .end et execute les choses avec le asyncet les await

--- backend/test.js
+++ backend/test.js
@@ -1,8 +1,11 @@
+// ce fichier est un test de connexion a la base de donnée (database.js est presque comme ce fichier  lui est une reel connexion mais unique sans les pool de connexion )
 require("dotenv").config()
 const mysql = require("mysql2/promise")
 
 const testConnection = async () => {
   const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env
+
+  // Le mot-clé await est utilisé à l'intérieur d'une fonction async pour attendre qu'une promesse soit résolue ou rejetée. Il met la fonction async en pause et permet à d'autres fonctions ou événements de s'exécuter pendant que la promesse est en attente.
 
   const connection = await mysql.createConnection({
     host: DB_HOST,
@@ -16,5 +19,19 @@ const testConnection = async () => {
   console.info("Connexion réussie !")
   await connection.end()
 }
+// après avoir établi une connexion à la base de données et affiché le message "Connexion réussie !", vous appelez await connection.end() pour fermer cette connexion.
+testConnection() // execution de la fonction declaré sans ce fichier
 
-testConnection()
+// JavaScript est un langage de programmation asynchrone, ce qui signifie que vous pouvez effectuer des opérations sans bloquer le fil d'exécution principal (comme le chargement d'une image pendant que le reste de votre page est toujours en cours d'exécution). Pour gérer cela, JavaScript utilise des promesses (Promises) qui sont un moyen de gérer les opérations asynchrones.
+
+// async :
+// Le mot-clé async est utilisé pour déclarer une fonction asynchrone, ce qui signifie que cette fonction renverra une promesse. Lorsque vous appelez cette fonction, elle exécute son corps et, lorsqu'elle se termine, elle renvoie une promesse résolue avec la valeur retournée par cette fonction.
+
+// Exemple :
+
+// async function fetchData() {
+//   let response = await fetch('https://api.example.com/data');
+//   let data = await response.json();
+//   return data;
+// }
+// Dans l'exemple ci-dessus, fetchData attend que la première promesse (l'appel à fetch) soit résolue avant de passer à la ligne suivante.
