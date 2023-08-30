1478ab0155e44af5c9a18e092112a98271ba8281
Djok64 <joakym.anc@gmail.com>
2023-08-30 12:27:56 +0200

    mise en place des routes pour que les futur requetes dans l'url qui seront déclenché par le front  active les fonctionnalités des controllers correspondant et communique ensuite avec la base de donnée et permette de realisé les action voulu  get/post/put/delete  + commentaire et explication sur tout le fichier router.js ainsi que details de la fonction des routes crée + commentaire et explication du fichier app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,22 @@
-// import some node modules for later
+// Vous importez deux modules Node.js.
 
+// File System, qui vous permet d'interagir avec le système de fichiers de votre ordinateur (lire, écrire, etc.):
 const fs = require("node:fs")
+// path est utilisé pour gérer les chemins de fichiers:
 const path = require("node:path")
 
-// create express app
+// Vous importez le module Express, un framework pour construire des applications web et API en Node.js. app est une instance de votre application Express:
 
 const express = require("express")
 
 const app = express()
 
-// use some application-level middlewares
-
+// Vous ajoutez un middleware qui analyse les requêtes entrantes avec des payloads JSON et les transforme en objets JavaScript accessibles via req.body:
 app.use(express.json())
-
+// Vous importez le middleware CORS (Cross-Origin Resource Sharing). CORS est une sécurité dans les navigateurs web qui empêche les requêtes HTTP d'être effectuées entre différents domaines, sauf si le domaine cible le permet:
 const cors = require("cors")
 
+// Vous ajoutez le middleware CORS à votre application. L'option origin détermine quel domaine est autorisé à accéder à votre API. Si process.env.FRONTEND_URL n'est pas défini, il utilisera "http://localhost:3000" comme valeur par défaut. optionsSuccessStatus est le code de statut à envoyer pour les requêtes OPTIONS réussies:
 app.use(
   cors({
     origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
@@ -22,18 +24,14 @@ app.use(
   })
 )
 
-// import and mount the API routes
-
+// Vous importez votre module de routage (défini ailleurs dans votre projet) et l'ajoutez à votre application. Ce module détermine comment répondre aux différentes requêtes HTTP adressées à votre API:
 const router = require("./router")
-
 app.use(router)
 
-// serve the `backend/public` folder for public resources
-
+// Vous configurez votre application pour servir des fichiers statiques (comme des images ou des fichiers CSS) depuis un dossier public dans le dossier parent de votre fichier actuel. __dirname est une variable globale en Node.js qui donne le chemin du répertoire du fichier en cours d'exécution:
 app.use(express.static(path.join(__dirname, "../public")))
 
-// serve REACT APP
-
+// Vous définissez le chemin vers le fichier index.html de votre application React. Vous supposez que votre application React est construite et ses fichiers sont dans le dossier "dist" sous un dossier "frontend" à la racine de votre projet:
 const reactIndexFile = path.join(
   __dirname,
   "..",
@@ -43,18 +41,17 @@ const reactIndexFile = path.join(
   "index.html"
 )
 
+// Si le fichier index.html de votre application React existe (c'est-à-dire si vous avez construit votre application React), alors:
 if (fs.existsSync(reactIndexFile)) {
-  // serve REACT resources
-
+  // Servez tous les fichiers statiques de votre application React:
   app.use(express.static(path.join(__dirname, "..", "..", "frontend", "dist")))
 
-  // redirect all requests to the REACT index file
-
+  // Pour toutes les autres requêtes (ce qui est signifié par "*"), renvoyez le fichier index.html. C'est une technique courante pour les applications React à page unique (Single Page Applications ou SPA) où React Router gère le routage côté client:
   app.get("*", (req, res) => {
     res.sendFile(reactIndexFile)
   })
 }
 
 // ready to export
-
+// Vous exportez l'instance de votre application Express pour pouvoir l'utiliser ailleurs dans votre projet, probablement dans un fichier où vous démarrez votre serveur.
 module.exports = app
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,10 +1,41 @@
+// importer les modules nécessaires
 const express = require("express")
 
 const router = express.Router()
+// ce sont les controller du template :
+const userControllers = require("../controllers/userControllers")
+const noteControllers = require("../controllers/noteControllers")
 
 const itemControllers = require("./controllers/itemControllers")
 const charactersControllers = require("./controllers/charactersControllers")
 
+// CREATION DES ROUTES POUR LA TABLE users :
+
+// Pour récupérer tous les utilisateurs de la table users:
+router.get("/users", userControllers.browse)
+// Pour récupérer un utilisateur spécifique par ID de la table users:
+router.get("/users/:id", userControllers.read)
+// Pour ajouter un nouvel utilisateur a la table users:
+router.post("/users", userControllers.add)
+// Pour mettre à jour un utilisateur de la table users:
+router.put("/users/:id", userControllers.edit)
+// Pour supprimer un utilisateur de la table users:
+router.delete("/users/:id", userControllers.destroy)
+
+// CREATION DES ROUTES POUR LA TABLE notes :
+
+// Pour récupérer tout les notes :
+router.get("/notes", noteControllers.browse)
+// Pour récupérer une note spécifique par son ID:
+router.get("/notes/:id", noteControllers.read)
+// Pour ajouter une nouvelle note:
+router.post("/notes", noteControllers.add)
+// Pour mettre a jour une note:
+router.put("/notes/:id", noteControllers.edit)
+// Pour supprimer une note :
+router.delete("/notes/:id", noteControllers.destroy)
+
+// exemple des controllers de  bases du template :
 router.get("/characters", charactersControllers.browse)
 router.get("/characters/:id", charactersControllers.read)
 router.post("/characters", charactersControllers.add)
@@ -17,4 +48,34 @@ router.put("/items/:id", itemControllers.edit)
 router.post("/items", itemControllers.add)
 router.delete("/items/:id", itemControllers.destroy)
 
+// exportation du fichier pour pouvoir l'utiliser dans un autre fichier
 module.exports = router
+
+// EXPLICATIONS SUR LES ROUTES EN DETAIL:
+
+// Fonctionnement des routes
+// Chaque ligne que tu as partagée définit une "route" dans ton serveur Express. Une route est essentiellement une combinaison d'une URL et d'une méthode HTTP. Lorsqu'une requête arrive à ton serveur avec cette combinaison spécifique, le serveur sait quelle action il doit exécuter.
+
+// Déclenchement des routes
+// Imaginons que tu aies un serveur fonctionnant à l'adresse http://monserveur.com.
+
+// Si quelqu'un va sur http://monserveur.com/notes avec son navigateur (ce qui équivaut à une requête GET), la première route (router.get("/notes", noteControllers.browse)) se déclenche. La fonction noteControllers.browse est exécutée pour traiter cette requête.
+
+// Si quelqu'un utilise un client API comme Postman ou fait une requête programmée pour obtenir http://monserveur.com/notes/123, la route avec :id (router.get("/notes/:id", noteControllers.read)) est déclenchée, et noteControllers.read est exécutée. Ici, 123 est passé en tant que paramètre id.
+
+// Middlewares et contrôleurs
+// Dans Express, tout est middleware. Les routes sont simplement des middlewares qui se déclenchent seulement si la combinaison URL/méthode HTTP correspond.
+
+// Le noteControllers.browse, noteControllers.read, etc., sont des fonctions (souvent appelées actions de contrôleur) qui traitent la requête et envoient une réponse. Ces fonctions reçoivent généralement trois paramètres principaux:
+
+// req (Requête): contient toutes les informations sur la requête entrante, comme les paramètres d'URL, les données du corps de la requête, les en-têtes, etc.
+// res (Réponse): un objet utilisé pour envoyer des réponses au client. Par exemple, si tu veux envoyer un message "Salut!" comme réponse, tu ferais quelque chose comme res.send("Salut!") dans ton contrôleur.
+// next: une fonction qui, lorsqu'elle est appelée, passe le contrôle au prochain middleware. Si tu ne l'appelles pas, le cycle de requête/réponse se termine là.
+// Ordre des routes
+// L'ordre dans lequel tu définis tes routes est important. Express utilisera la première route correspondante qu'il trouve. Si tu avais défini la route /notes/:id avant /notes, et qu'un utilisateur demandait /notes, Express traiterait cela comme s'il cherchait une note avec l'ID "notes", ce qui n'est probablement pas ce que tu veux.
+
+// Paramètres d'URL
+// Les :param dans les routes, comme :id, sont des "paramètres d'URL". Ils correspondent à n'importe quelle valeur. Ainsi, /notes/123 et /notes/abc correspondent tous deux à la route /notes/:id, et tu peux accéder à la valeur spécifique (comme 123 ou abc) dans ton contrôleur via req.params.id.
+
+// Résumé
+// Chaque fois qu'une requête arrive à ton serveur, Express vérifie si l'URL et la méthode HTTP correspondent à l'une des routes définies. Si c'est le cas, le contrôleur correspondant est exécuté pour traiter la requête et envoyer une réponse.
